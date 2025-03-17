08e81f12336464d213a0c526ee4c9383bcfa209a
MassAlam. <satria26vs@gmail.com>
2025-03-18 05:38:43 +0700

    Delete Kelas/PHP/coffee_shop directory

--- Kelas/PHP/coffee_shop/create_hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Ganti 'password' dengan password yang ingin Anda hash
-$password = 'password'; // Ganti dengan password yang Anda inginkan
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo "Hashed Password: " . $hashedPassword;
-?>
--- Kelas/PHP/coffee_shop/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: index.php');
-exit();
-?><script src="assets/js/script.js"></script>
